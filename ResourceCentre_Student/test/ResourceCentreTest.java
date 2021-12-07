1ef5b54f49ea9294d1a7f9be59e80992c5ed1478
Shawn Han <Shawn Han@DESKTOP-SC0NVOH>
2021-12-07 10:38:28 +0800

    Shawn and Andi is here!!!

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -55,7 +55,7 @@ public class ResourceCentreTest {
 	
 	@Test
 	public void testAddChromebook() {
-		//Andi is here
+		//Shawn and Andi is here
 		//fail("Not yet implemented");
 		// write your code here
 		// Item list is not null, so that can add a new item
